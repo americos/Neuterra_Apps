463ad2c88741f70afa9bfa4aa2f48f74f04d5219
Americo Savinon <americos@gmail.com>
2011-10-05 13:18:45 -0400

    Initial commit

---
+++ Neuterra_WorReport/js/app.js
@@ -0,0 +1,29 @@
+Sample.Hello = function( app ) {
+	window.chart = new Highcharts.StockChart({
+	    chart: {
+	        renderTo: 'container'
+	    },
+	    
+	    rangeSelector: {
+	        selected: 1
+	    },
+	    
+	    title: {
+	        text: 'USD to EUR exchange rate'
+	    },
+	    
+	    xAxis: {
+	        maxZoom: 14 * 24 * 3600000 // fourteen days
+	    },
+	    yAxis: {
+	        title: {
+	            text: 'Exchange rate'
+	        }
+	    },
+		
+	    series: [{
+	        name: 'USD to EUR',
+	        data: usdeur
+	    }]
+	});
+};
