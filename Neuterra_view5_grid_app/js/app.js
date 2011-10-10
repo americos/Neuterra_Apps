ffced3df1d784387e5b9148730939dd34d08693b
Americo Savinon <americos@gmail.com>
2011-10-10 16:02:10 -0400

    Adding columns and data to grouping grid

--- Neuterra_view5_grid_app/js/app.js
+++ Neuterra_view5_grid_app/js/app.js
@@ -3,222 +3,107 @@ Sample.Hello = function( app ) {
 	Ext.require(['Ext.data.*', 'Ext.grid.*']);
 	Ext.onReady(function() {
 	    // wrapped in closure to prevent global vars.
-	    Ext.define('Restaurant', {
+	    Ext.define('Payor', {
 	        extend: 'Ext.data.Model',
-	        fields: ['name', 'cuisine']
+	        fields: ['name', 'specialty', 'cases']
 	    });
 
-	    var Restaurants = Ext.create('Ext.data.Store', {
-	        storeId: 'restaraunts',
-	        model: 'Restaurant',
-	        sorters: ['cuisine','name'],
-	        groupField: 'cuisine',
+	    var Payors = Ext.create('Ext.data.Store', {
+	        storeId: 'payors',
+	        model: 'Payor',
+	        sorters: ['name','specialty'],
+	        groupField: 'name',
 	        data: [{
-	            name: 'Cheesecake Factory',
-	            cuisine: 'American'
-	        },{
-	            name: 'University Cafe',
-	            cuisine: 'American'
-	        },{
-	            name: 'Slider Bar',
-	            cuisine: 'American'
-	        },{
-	            name: 'Shokolaat',
-	            cuisine: 'American'
-	        },{
-	            name: 'Gordon Biersch',
-	            cuisine: 'American'
-	        },{
-	            name: 'Crepevine',
-	            cuisine: 'American'
-	        },{
-	            name: 'Creamery',
-	            cuisine: 'American'
-	        },{
-	            name: 'Old Pro',
-	            cuisine: 'American'
-	        },{
-	            name: 'Nola\'s',
-	            cuisine: 'Cajun'
-	        },{
-	            name: 'House of Bagels',
-	            cuisine: 'Bagels'
-	        },{
-	            name: 'The Prolific Oven',
-	            cuisine: 'Sandwiches'
-	        },{
-	            name: 'La Strada',
-	            cuisine: 'Italian'
-	        },{
-	            name: 'Buca di Beppo',
-	            cuisine: 'Italian'
-	        },{
-	            name: 'Pasta?',
-	            cuisine: 'Italian'
-	        },{
-	            name: 'Madame Tam',
-	            cuisine: 'Asian'
-	        },{
-	            name: 'Sprout Cafe',
-	            cuisine: 'Salad'
-	        },{
-	            name: 'Pluto\'s',
-	            cuisine: 'Salad'
-	        },{
-	            name: 'Junoon',
-	            cuisine: 'Indian'
-	        },{
-	            name: 'Bistro Maxine',
-	            cuisine: 'French'
-	        },{
-	            name: 'Three Seasons',
-	            cuisine: 'Vietnamese'
-	        },{
-	            name: 'Sancho\'s Taquira',
-	            cuisine: 'Mexican'
-	        },{
-	            name: 'Reposado',
-	            cuisine: 'Mexican'
-	        },{
-	            name: 'Siam Royal',
-	            cuisine: 'Thai'
-	        },{
-	            name: 'Krung Siam',
-	            cuisine: 'Thai'
-	        },{
-	            name: 'Thaiphoon',
-	            cuisine: 'Thai'
-	        },{
-	            name: 'Tamarine',
-	            cuisine: 'Vietnamese'
-	        },{
-	            name: 'Joya',
-	            cuisine: 'Tapas'
-	        },{
-	            name: 'Jing Jing',
-	            cuisine: 'Chinese'
-	        },{
-	            name: 'Patxi\'s Pizza',
-	            cuisine: 'Pizza'
-	        },{
-	            name: 'Evvia Estiatorio',
-	            cuisine: 'Mediterranean'
-	        },{
-	            name: 'Cafe 220',
-	            cuisine: 'Mediterranean'
-	        },{
-	            name: 'Cafe Renaissance',
-	            cuisine: 'Mediterranean'
-	        },{
-	            name: 'Kan Zeman',
-	            cuisine: 'Mediterranean'
-	        },{
-	            name: 'Gyros-Gyros',
-	            cuisine: 'Mediterranean'
-	        },{
-	            name: 'Mango Caribbean Cafe',
-	            cuisine: 'Caribbean'
-	        },{
-	            name: 'Coconuts Caribbean Restaurant &amp; Bar',
-	            cuisine: 'Caribbean'
-	        },{
-	            name: 'Rose &amp; Crown',
-	            cuisine: 'English'
-	        },{
-	            name: 'Baklava',
-	            cuisine: 'Mediterranean'
-	        },{
-	            name: 'Mandarin Gourmet',
-	            cuisine: 'Chinese'
-	        },{
-	            name: 'Bangkok Cuisine',
-	            cuisine: 'Thai'
-	        },{
-	            name: 'Darbar Indian Cuisine',
-	            cuisine: 'Indian'
-	        },{
-	            name: 'Mantra',
-	            cuisine: 'Indian'
-	        },{
-	            name: 'Janta',
-	            cuisine: 'Indian'
-	        },{
-	            name: 'Hyderabad House',
-	            cuisine: 'Indian'
-	        },{
-	            name: 'Starbucks',
-	            cuisine: 'Coffee'
-	        },{
-	            name: 'Peet\'s Coffee',
-	            cuisine: 'Coffee'
-	        },{
-	            name: 'Coupa Cafe',
-	            cuisine: 'Coffee'
-	        },{
-	            name: 'Lytton Coffee Company',
-	            cuisine: 'Coffee'
-	        },{
-	            name: 'Il Fornaio',
-	            cuisine: 'Italian'
-	        },{
-	            name: 'Lavanda',
-	            cuisine: 'Mediterranean'
-	        },{
-	            name: 'MacArthur Park',
-	            cuisine: 'American'
-	        },{
-	            name: 'St Michael\'s Alley',
-	            cuisine: 'Californian'
-	        },{
-	            name: 'Cafe Renzo',
-	            cuisine: 'Italian'
-	        },{
-	            name: 'Osteria',
-	            cuisine: 'Italian'
-	        },{
-	            name: 'Vero',
-	            cuisine: 'Italian'
-	        },{
-	            name: 'Cafe Renzo',
-	            cuisine: 'Italian'
-	        },{
-	            name: 'Miyake',
-	            cuisine: 'Sushi'
-	        },{
-	            name: 'Sushi Tomo',
-	            cuisine: 'Sushi'
-	        },{
-	            name: 'Kanpai',
-	            cuisine: 'Sushi'
-	        },{
-	            name: 'Pizza My Heart',
-	            cuisine: 'Pizza'
-	        },{
-	            name: 'New York Pizza',
-	            cuisine: 'Pizza'
-	        },{
-	            name: 'California Pizza Kitchen',
-	            cuisine: 'Pizza'
-	        },{
-	            name: 'Round Table',
-	            cuisine: 'Pizza'
-	        },{
-	            name: 'Loving Hut',
-	            cuisine: 'Vegan'
-	        },{
-	            name: 'Garden Fresh',
-	            cuisine: 'Vegan'
-	        },{
-	            name: 'Cafe Epi',
-	            cuisine: 'French'
-	        },{
-	            name: 'Tai Pan',
-	            cuisine: 'Chinese'
+	            name: 'Patient Responsible',
+	            specialty: 'Ophthalmology',
+				physician: 'EVERSON, MARSHALL, MD',
+				cases : 240,
+				gr: 1500,
+				nr: 2300
+	        },{
+	            name: 'AAA',
+	            specialty: 'Orthopedic Surgery',
+				physician: 'SCHAFFHAUSEN, JAMES MD',	
+				cases : 100,
+				gr: 1200,
+				nr: 2100
+	        },
+			{
+	            name: 'Patient Responsible',
+	            specialty: 'Orthopedic Surgery',
+				physician: 'OLMSTED 1, STEPHEN, MD',
+				cases : 200,
+				gr: 800,
+				nr: 300
+	        }
+			,{
+	            name: 'AETNA (AETNASC)',
+	            specialty: 'Orthopedic Surgery',
+				physician: 'EBELING, PATRICK, MD',
+				cases : 340,
+				gr: 500,
+				nr: 2100
+	       	},
+			{
+	            name: 'AETNA (AETNASC)',
+	            specialty: 'Orthopedic Surgery',
+				physician: 'HADLEY, RICAHRD MD',
+				cases : 160,
+				gr: 1600,
+				nr: 2300
+	       	},
+			{
+	            name: 'AETNA (AETNASC)',
+	            specialty: 'Orthopedic Surgery',
+				physician: 'JOHNSON, NEIL, MD',
+				cases : 150,
+				gr: 800,
+				nr: 1000
+	       	},
+			{
+	            name: 'AETNA (AETNASC)',
+	            specialty: 'Orthopedic Surgery',
+				physician: 'JOHNSON, NEIL, MD',
+				cases : 130,
+				gr: 4500,
+				nr: 8300
+	       	},
+			{
+	            name: 'AETNA (AETNASC)',
+	            specialty: 'Orthopedic Surgery',
+				physician: 'OLMSTED 1, STEPHEN, MD',
+				cases : 130,
+				gr: 7500,
+				nr: 7300
+	       	},
+			{
+	            name: 'AETNA (AETNASC)',
+	            specialty: 'Orthopedic Surgery',
+				physician: 'SCHAFFHAUSEN, JAMES MD',
+				cases : 350,
+				gr: 3500,
+				nr: 7300
+	       	},
+			{
+	            name: 'AAA',
+	            specialty: 'Podiatrist',
+				physician: 'SCHAFFHAUSEN, JAMES MD',
+				cases : 100,
+				gr: 5500,
+				nr: 7300
+	        },
+				{
+	            name: 'AIG CLAIM SERVICES (AIG)',
+	            specialty: 'Orthopedic Surgery',
+				physician: 'OLMSTED 1, STEPHEN, MD',
+				cases : 350,
+				gr: 7500,
+				nr: 9300
 	        }]
 	    });
 
 	    var groupingFeature = Ext.create('Ext.grid.feature.Grouping',{
-	        groupHeaderTpl: 'Cuisine: {name} ({rows.length} Item{[values.rows.length > 1 ? "s" : ""]})'
+	        groupHeaderTpl: '{name} ({rows.length} Item{[values.rows.length > 1 ? "s" : ""]})'
 	    });
 
 	    var grid = Ext.create('Ext.grid.Panel', {
@@ -226,27 +111,36 @@ Sample.Hello = function( app ) {
 	        collapsible: true,
 	        iconCls: 'icon-grid',
 	        frame: true,
-	        store: Restaurants,
+	        store: Payors,
 	        width: 600,
 	        height: 400,
-	        title: 'Restaurants',
+	        title: 'Payors by Specialty',
 	        features: [groupingFeature],
 	        columns: [{
-	            text: 'Name',
+	            text: 'Physician',
 	            flex: 1,
-	            dataIndex: 'name'
+	            dataIndex: 'physician'
 	        },{
-	            text: 'Cuisine',
+	            text: 'Specialty',
 	            flex: 1,
-	            dataIndex: 'cuisine'
-	        }],
-	        fbar  : ['->', {
-	            text:'Clear Grouping',
-	            iconCls: 'icon-clear-group',
-	            handler : function(){
-	                groupingFeature.disable();
-	            }
-	        }]
+	            dataIndex: 'specialty'
+	        },{
+	            text: 'Cases',
+	            flex: 1,
+	            dataIndex: 'cases',
+				format:'0,000'
+	        },	{
+		            text: 'Sum of GR per Case',
+		            flex: 1,
+		            dataIndex: 'gr',
+					renderer: Ext.util.Format.usMoney
+		        },
+				{
+		            text: 'Sum of NR per Case',
+		            flex: 1,
+					dataIndex:'nr',
+		            renderer: Ext.util.Format.usMoney
+		        }]
 	    });
 	});
 };
